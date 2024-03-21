eb90ce8721ef31fff1dc0d28ec41e2ce4258b60f
WelberLopes <deunatelhacontato01@gmail.com>
2024-03-20 21:31:51 -0300

    Second commit

---
+++ Banco/Conta.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+
+namespace Banco {
+    internal class Conta {
+
+        private string _nomeTitular;
+        public string _nConta { get; private set; }
+        public double _saldo { get; private set; }
+
+        public Conta() { }
+        public Conta(string Nome,string Conta, double Deposito) {
+
+            _nomeTitular = Nome;
+            _nConta = Conta;    
+            _saldo = Deposito;
+        }
+
+        private void SetNumeroConta(string nConta) {
+            _nConta = nConta;
+        }
+
+        public void SetNomeTitular(string nomeTitular) {
+            _nomeTitular = nomeTitular;
+        }
+
+        public static void Deposito(double Montante, Conta c1) {
+            c1._saldo += Montante;
+        }
+        public static void Saque(double Montante, Conta c1) {
+            c1._saldo = c1._saldo - Montante - 5.00;
+        }
+
+        public override string ToString() {
+            return "Dados da conta:\n" +
+                   "Conta: " + _nConta + "\nTitular: " + _nomeTitular + "\nSaldo: R$" + _saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Banco/Program.cs
@@ -0,0 +1,112 @@
+﻿using System.Globalization;
+
+
+namespace Banco {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+
+            Console.WriteLine("------------- T O D A Y $ B A N K -------------");
+
+            Console.WriteLine("Ecolha a operação que deseja realizar: ");
+            Console.WriteLine(" ");
+            Console.WriteLine("1- Abrir conta bancária.  2- Sair.");
+            Console.WriteLine(" ");
+
+            int escolha = int.Parse(Console.ReadLine());
+
+            if (escolha == 1) {
+
+                Console.WriteLine("Digite o nome do titular: ");
+
+                string titular = Console.ReadLine();
+
+                Console.WriteLine("Digite seu CPF será utilizado como identificação da conta: ");
+
+                string CPF = Console.ReadLine();
+
+                Console.WriteLine("Haverá um depósito inicial (s/n)?");
+
+
+                string escolhaMenu2 = Console.ReadLine();
+                double deposito = 0;
+
+                Conta c1 = new Conta(titular, CPF, deposito);
+
+                if (escolhaMenu2 == "s") {
+
+                    Console.WriteLine("Insira o valor do depósito inicial: ");
+
+                    deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    Conta.Deposito(deposito, c1);
+                    Console.Clear();
+
+                    Console.WriteLine(c1);
+                    Console.ReadKey();
+
+                }
+
+
+                // Console.WriteLine("Deseja fazer Um depósito ou saque?");
+                // Console.WriteLine(" 1- Depósito. 2- Saque.");
+
+                // int escolha2 = int.Parse(Console.ReadLine());
+
+                //if (escolha2 == 1) {
+
+                Console.WriteLine("Digite o valor a ser depositado: ");
+                double depo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Conta.Deposito(depo, c1);
+
+                Console.Clear();
+
+                Console.WriteLine(c1);
+
+                Console.ReadKey();
+
+
+                // }
+                //else if (escolha2 == 2) {
+
+                Console.WriteLine("Digite o valor a ser sacado: ");
+                double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Conta.Saque(saque, c1);
+                Console.Clear();
+                Console.WriteLine(c1);
+                Console.WriteLine(" ");
+
+                Console.WriteLine("Volte Sempre!");
+                Console.ReadKey();
+            }
+            if (escolha == 2) {
+
+                Console.WriteLine("Volte Sempre!");
+
+                Console.ReadKey();
+
+            }
+            else if (escolha != 1 && escolha != 2) {
+                Console.WriteLine("Opção Invalida! Pressione qualquer tecla!");
+                Console.ReadKey();
+                
+                Console.Clear();
+
+                Main(args);
+            }
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
